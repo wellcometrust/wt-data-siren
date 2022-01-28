2dbc301ac38974e50b6ec970b537853de355434c
Jeff Uren <jduren@protonmail.com>
2022-01-28 11:57:35 +0000

    config param is deprecated

--- models/base/datasiren_audit_results.sql
+++ models/base/datasiren_audit_results.sql
@@ -1,28 +1,29 @@
-{{ config(
-        materialized='incremental',
-        full_refresh="false"
-    )
+{{ config({
+        "materialized": "incremental"
+    })
 }}
 
-{% for sensor in dbt_utils.get_relations_by_prefix(var('datasiren:schema_name'), 'datasiren_', exclude='datairen_audit_results', database=this.database) %}
+{% for sensor in dbt_utils.get_relations_by_prefix(var('datasiren:schema_name'), 'datasiren_', exclude='datasiren_audit_results', database=this.database) %}
 
     SELECT * 
     FROM {{ sensor }}
 
     {% if is_incremental() %}
         WHERE time_detected > 
-            ( 
-                SELECT IFNULL(MAX(time_detected), '1900-01-01 00:00:00.000 +0000')
-                FROM {{ this }}
-                WHERE sensor_name in (
-                    SELECT DISTINCT sensor_name
-                    FROM {{ sensor }} 
-                )
+          (
+            SELECT IFNULL(MAX(time_detected),'1900-01-01 00:00:00.000 +0000') 
+            FROM {{ this }} 
+            WHERE sensor_name in (
+              SELECT DISTINCT sensor_name 
+              FROM {{ sensor }}
             )
+          ) 
     {% endif %}
 
     {% if not loop.last %}
-        UNION ALL 
+        UNION ALL
     {% endif %}
+    
 
 {% endfor -%}
+
