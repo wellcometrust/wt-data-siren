86ae8e5d39798e4e88703c043aed66d03f5e3ea0
Jeff Uren <jduren@protonmail.com>
2022-02-23 12:00:12 +0000

    Changing to use datasiren materialized view

--- macros/sensor_templates/postgres__get_columns_by_type.sql
+++ macros/sensor_templates/postgres__get_columns_by_type.sql
@@ -1,17 +1,14 @@
 {% macro postgres__get_columns_by_type(database_to_scan, column_type, sample_rule, schema_name = None) %}
 
-    CREATE TEMP TABLE tmp_columns_table AS
-        SELECT 
-            table_schema        AS table_schema,
-            table_name          AS table_name,
-            column_name         AS column_name
-        FROM "{{ database_to_scan }}".information_schema.columns
-        WHERE LOWER(data_type) = LOWER('{{column_type}}')
-        {% if schema_name %}
-            AND LOWER(table_schema) = '{{schema_name|lower}}'
-        {% endif %}
-
-    SELECT * FROM tmp_columns_table TABLESAMPLE BERNOULLI ({{sample_rule}})
+    SELECT 
+      table_schema        AS table_schema,
+      table_name          AS table_name,
+      column_name         AS column_name
+    FROM "{{ database_to_scan }}".datasiren.datasiren_table_columns TABLESAMPLE BERNOULLI ({{sample_rule}})
+    WHERE LOWER(data_type) = LOWER('{{column_type}}')
+    {% if schema_name %}
+      AND LOWER(table_schema) = '{{schema_name|lower}}'
+    {% endif %}
 
 
 {%- endmacro -%}
