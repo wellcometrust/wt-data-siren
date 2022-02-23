ad221cf469e76c10aede21dba56c9b8feb5f9a5d
Jeff Uren <jduren@protonmail.com>
2022-02-23 10:36:28 +0000

    Trying TABLESAMPLE with a temp table as TABLESAMPLE does not work against system tables

--- macros/sensor_templates/postgres__get_columns_by_type.sql
+++ macros/sensor_templates/postgres__get_columns_by_type.sql
@@ -1,14 +1,18 @@
 {% macro postgres__get_columns_by_type(database_to_scan, column_type, sample_rule, schema_name = None) %}
 
-    SELECT 
-      table_schema        AS table_schema,
-      table_name          AS table_name,
-      column_name         AS column_name
-    FROM "{{ database_to_scan }}".information_schema.columns TABLESAMPLE BERNOULLI ({{sample_rule}})
-    WHERE LOWER(data_type) = LOWER('{{column_type}}')
-    {% if schema_name %}
-      AND LOWER(table_schema) = '{{schema_name|lower}}'
-    {% endif %}
+    CREATE TEMP TABLE tmp_columns_table AS
+        SELECT 
+            table_schema        AS table_schema,
+            table_name          AS table_name,
+            column_name         AS column_name
+        FROM "{{ database_to_scan }}".information_schema.columns
+        WHERE LOWER(data_type) = LOWER('{{column_type}}')
+        {% if schema_name %}
+            AND LOWER(table_schema) = '{{schema_name|lower}}'
+        {% endif %}
+
+    SELECT * FROM tmp_columns_table TABLESAMPLE BERNOULLI ({{sample_rule}})
+
 
 {%- endmacro -%}
 
