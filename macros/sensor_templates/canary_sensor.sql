181f7c863d342c9337b64f0a1826d9bdbc688fb0
Jeff Uren <jduren@protonmail.com>
2022-02-23 14:59:55 +0000

    Added comment to canary

--- macros/sensor_templates/canary_sensor.sql
+++ macros/sensor_templates/canary_sensor.sql
@@ -1,5 +1,11 @@
 {% macro canary_sensor(database) %}
 
+    {# 
+        This does not return anything specific, it effectively just
+        gets a single column from the database of really any column_name
+        as a result to indicate that the canary is still alive (i.e. the database).
+    #}
+
     {{ adapter.dispatch('find_columns_with_names_like', 'datasiren')('canary_sensor', '%', database|trim) }}
     LIMIT 1
 
